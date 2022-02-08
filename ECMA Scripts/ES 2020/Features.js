5ba20a4c2e4395ea76a8f8a37f2b5dcc1272b58f
Ayush <ayush.kanduri@gmail.com>
2022-02-09 01:27:27 +0530

    optional chaining

--- ECMA Scripts/ES 2020/Features.js
+++ ECMA Scripts/ES 2020/Features.js
@@ -1,4 +1,5 @@
 //------------------------------------------------------------
+//------------------------------------------------------------
 // 1. Global This Keyword - ES2020 - Features //
 //Window can't be used for NodeJS, so 'globalThis' is a common Global Window for all the codes.
 // console.log(this); - doesn't work
@@ -6,6 +7,7 @@
 // console.log(window); - doesn't work
 // console.log(frame); - doesn't work
 console.log(globalThis); // works
+//------------------------------------------------------------
 //------------------------------------------------------------
 // 2. Promises.allSettled() - ES2020 - Features //
 //Checks, If all the promises have been settled or not. It returns the value signifying the completion of all the promises despite of it being rejected or resolved.
@@ -30,4 +32,43 @@ Promise.allSettled([promise1, promise2, promise3]).then((result) => {
 	console.log(result);
 }); // Returns an array of objects with the status of all the promises after their completion.
 //------------------------------------------------------------
-// 2. Promises.allSettled() - ES2020 - Features //
+//------------------------------------------------------------
+// 3. Optional Chaining - ES2020 - Features //
+const creature = {
+	mammal: {
+		human: {
+			male: {
+				name: "Ayush",
+			},
+		},
+	},
+};
+
+// const name1 = creature.mammal.human.male.name;
+// // const name2 = creature.mammal.human.female.name; - throws an error Cannot read property 'name' of undefined
+
+// if (
+// 	creature &&
+// 	creature.mammal &&
+// 	creature.mammal.human &&
+// 	creature.mammal.human.male &&
+// 	creature.mammal.human.male.name
+// ) {
+// 	console.log(name1);
+// 	// console.log(name2);
+// 	console.log(true);
+// }
+
+// If the property doesn't exist then the . operator gives the error in accessing the undefined data.
+// But optional chaining makes it easier to access the object properties by accessing even the undefined if the property doesn't exist. That too without an error.
+
+const name1 = creature?.mammal?.human?.male?.name;
+const name2 = creature?.mammal?.human?.female?.name;
+
+if (name1) {
+	console.log(name1);
+	console.log(name2);
+	console.log(true);
+}
+//------------------------------------------------------------
+//------------------------------------------------------------
