933d1a9ad5b408226e6ca514afdb027ff2157c32
Ayush <ayush.kanduri@gmail.com>
2022-02-09 01:03:46 +0530

    promise allsettled

--- ECMA Scripts/ES 2020/Features.js
+++ ECMA Scripts/ES 2020/Features.js
@@ -1,5 +1,5 @@
 //------------------------------------------------------------
-// 1. Global This Keyword - ES2020 - Features
+// 1. Global This Keyword - ES2020 - Features //
 //Window can't be used for NodeJS, so 'globalThis' is a common Global Window for all the codes.
 // console.log(this); - doesn't work
 // console.log(self); - doesn't work
@@ -7,4 +7,27 @@
 // console.log(frame); - doesn't work
 console.log(globalThis); // works
 //------------------------------------------------------------
-//Checks, If all the promises have been settled or not.
+// 2. Promises.allSettled() - ES2020 - Features //
+//Checks, If all the promises have been settled or not. It returns the value signifying the completion of all the promises despite of it being rejected or resolved.
+const promise1 = new Promise((resolve, reject) => {
+	setTimeout(resolve("Promise 1"), 1000);
+});
+const promise2 = new Promise((resolve, reject) => {
+	setTimeout(reject("Promise 2"), 2000);
+});
+const promise3 = new Promise((resolve, reject) => {
+	setTimeout(resolve("Promise 3"), 3000);
+});
+
+// Promise.all([promise1, promise2, promise3])
+// 	.then((result) => {
+// 		console.log(result);
+// 	})
+// 	.catch((error) => {
+// 		console.log("Caught Exception of " + error);
+// 	}); - Throws error as Promise 2 is rejected.
+Promise.allSettled([promise1, promise2, promise3]).then((result) => {
+	console.log(result);
+}); // Returns an array of objects with the status of all the promises after their completion.
+//------------------------------------------------------------
+// 2. Promises.allSettled() - ES2020 - Features //
