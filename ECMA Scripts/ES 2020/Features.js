a1a41adb8e52914a9e5c2e82b4ea5301eece0431
Ayush <ayush.kanduri@gmail.com>
2022-02-09 00:39:29 +0530

    global this

---
+++ ECMA Scripts/ES 2020/Features.js
@@ -0,0 +1,10 @@
+//------------------------------------------------------------
+// 1. Global This Keyword - ES2020 - Features
+//Window can't be used for NodeJS, so 'globalThis' is a common Global Window for all the codes.
+// console.log(this); - doesn't work
+// console.log(self); - doesn't work
+// console.log(window); - doesn't work
+// console.log(frame); - doesn't work
+console.log(globalThis); // works
+//------------------------------------------------------------
+//Checks, If all the promises have been settled or not.
