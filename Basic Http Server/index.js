6d499bed3c333bb971cf01df1f28cf93d03b1dff
Ayush <ayush.kanduri@gmail.com>
2022-02-12 12:26:37 +0530

    basic http server

--- Basic Http Server/index.js
+++ Basic Http Server/index.js
@@ -1,7 +1,11 @@
+//Require the HTTP Module for creating & running the NodeJS Server
 const http = require("http");
+//Create Port
 const port = 8000;
+//Create NodeJS Server
 const server = http.createServer();
 
+//Run the NodeJS Server
 server.listen(port, (err) => {
 	if (err) {
 		console.log(err);
