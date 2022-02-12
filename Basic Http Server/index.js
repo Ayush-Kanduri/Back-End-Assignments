48d6e9381eb9c56545eaf00d42cbcee262015a27
Ayush <ayush.kanduri@gmail.com>
2022-02-12 12:46:17 +0530

    request handler added

--- Basic Http Server/index.js
+++ Basic Http Server/index.js
@@ -2,6 +2,15 @@
 const http = require("http");
 //Create Port
 const port = 8000;
+
+//Request Handler to handle the Request-Response in NodeJS
+const requestHandler = (request, response) => {
+	//url request coming to the server(our server) - Request URL Load
+	console.log(request.url);
+	//response sending to the client(browser)
+	response.end("Hello World");
+};
+
 //Create NodeJS Server
 const server = http.createServer();
 
