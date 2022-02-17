193a7d90176988700661f83c6348f6174d23a684
Ayush <ayush.kanduri@gmail.com>
2022-02-17 18:56:28 +0530

    Server Setup, Controller Setup, Template Engine Views Setup - Directory Path Setup

--- Contact List/index.js
+++ Contact List/index.js
@@ -2,16 +2,27 @@
 const express = require("express");
 //Create Port
 const port = 8000;
+//Require Module Path for Directory
+const path = require("path");
 //Create Express App for Request-Response Cycle & to create the Express Server
 const app = express();
 
+//Set Up Template Engine as EJS
+app.set("view engine", "ejs");
+//Set Up Template Engine Views Folder Path (..../views)
+app.set("views", path.join(__dirname, "views"));
+
 //Home Page Controller
 app.get("/", (request, response) => {
-	// //Send the Response to the clients
-	// response.send(`Express Server is Up & Running Successfully on Port ${port}`);
+	////Send the Response to the clients
+	//response.send(`Express Server is Up & Running Successfully on Port ${port}`);
 
-	//Request information from the clients
-	console.log(request);
+	//Global Dynamic Variable for directory path
+	console.log(__dirname);
+
+	////Request information from the clients
+	// console.log(request);
+
 	//Send the Response to the clients
 	response.send(
 		`<h1>Express Server is Up & Running Successfully on Port ${port}</h1>`
