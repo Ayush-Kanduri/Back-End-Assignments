07890b15f9eff4e1230b1f4a546cdfeda21b8f56
Ayush <ayush.kanduri@gmail.com>
2022-02-26 18:25:34 +0530

    Middleware & Assets added

--- Contact List/index.js
+++ Contact List/index.js
@@ -13,6 +13,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 //Middleware - URL Encoder
 app.use(express.urlencoded());
+//Middleware - Static Files
+app.use(express.static("assets"));
 
 //Contact List Array of Objects to store the data
 let contactList = [
